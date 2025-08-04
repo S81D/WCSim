5e93f6c805de58b8f4279cb39bf75e712d0af19d
Steven Doran <doran@iastate.edu>
2025-08-04 09:13:37 -0500

    Revert tilting + shifting changes

--- src/WCSimConstructANNIECylinderScan.cc
+++ src/WCSimConstructANNIECylinderScan.cc
@@ -232,20 +232,14 @@ G4cout<< " placing logicWCPMT: "<< thepmtname<< " at " << atankcollection << G4e
 	
 	// Create rotation matrices for the orientations of the PMTs
 	std::vector<G4RotationMatrix*> pmt_rotation_matrices;
-	std::vector<G4RotationMatrix*> tilted_pmt_rotation_matrices;
 	// Bottom PMTs have panel number 0
 	G4RotationMatrix *WCBottomCapRotation = new G4RotationMatrix();
 	pmt_rotation_matrices.push_back(WCBottomCapRotation);
-	tilted_pmt_rotation_matrices.push_back(WCBottomCapRotation);
 	// Barrel PMTs have panel numbers 1-8
 	for(int facei=0; facei<WCBarrelRingNPhi; facei++){
 		G4RotationMatrix* WCPMTRotationNext = new G4RotationMatrix(*WCPMTRotation);
-		G4RotationMatrix* WCPMTtiltRotationNext = new G4RotationMatrix(*WCPMTRotation);
 		WCPMTRotationNext->rotateX((dPhi*facei)-67.5*deg+180*deg);
-		WCPMTtiltRotationNext->rotateX((dPhi*facei)-67.5*deg+180*deg);
-		WCPMTtiltRotationNext->rotateY(-53*deg);
 		pmt_rotation_matrices.push_back(WCPMTRotationNext);
-		tilted_pmt_rotation_matrices.push_back(WCPMTtiltRotationNext);
 	}
 	// Top PMTs have panel number 9
 	G4RotationMatrix *WCTopCapRotation = new G4RotationMatrix();
@@ -260,7 +254,6 @@ G4cout<< " placing logicWCPMT: "<< thepmtname<< " at " << atankcollection << G4e
 	std::string next_pmt;
 	double pmt_x, pmt_y, pmt_z, pmt_dirx, pmt_diry, pmt_dirz;
 	double pmt_x_shift, pmt_y_shift, pmt_z_shift;
-	double tilt_pmt_x_shift, tilt_pmt_y_shift, tilt_pmt_z_shift;
 	int panel_nr, pmt_type;
 	int PMTID;
 	while (!pmt_position_file.eof()){
@@ -268,27 +261,14 @@ G4cout<< " placing logicWCPMT: "<< thepmtname<< " at " << atankcollection << G4e
 		if (pmt_position_file.eof()) break;
 		//G4cout << "Read in PMT "<<PMTID<<", panel nr: "<<panel_nr<<", Position ("<<pmt_x<<","<<pmt_y<<","<<pmt_z<<"), PMT type: "<<pmt_type<<G4endl;
 		G4LogicalVolume *logicWCPMT = logicWCPMTs.at(pmt_type);
-		G4RotationMatrix *tilt_pmt_rot = tilted_pmt_rotation_matrices.at(panel_nr); 
 		G4RotationMatrix *pmt_rot = pmt_rotation_matrices.at(panel_nr);
 		pmt_x_shift = pmt_x*cm;
 		pmt_y_shift = (168.1-pmt_z)*cm;
 		pmt_z_shift = ((pmt_y+14.45))*cm;
-		tilt_pmt_z_shift = (pmt_y+14.45+13.9)*cm;
 		//pmt_z_shift = ((pmt_y+14.45)-InnerStructureCentreOffset/10.)*cm;
 		G4cout <<"Edited PMT position ("<<pmt_x_shift<<","<<pmt_y_shift<<","<<pmt_z_shift<<")"<<G4endl;
 		G4ThreeVector PMTPosition(pmt_x_shift,pmt_y_shift,pmt_z_shift);
-		G4ThreeVector PMTPosition_tilt(pmt_x_shift,pmt_y_shift,tilt_pmt_z_shift);
-
-		if ((pmt_type == 3)||(pmt_type == 0 && panel_nr != 0)){
-		G4VPhysicalVolume *physicalWCPMT = new G4PVPlacement(tilt_pmt_rot,	//its rotation
-															PMTPosition_tilt,		//its position
-															logicWCPMT,			//its logical volume
-															"WCPMT",			//its name
-															logicWCBarrel,		//its mother volume
-															false,				//no boolean operations
-															PMTID,				//ID for this PMT (=channelkey in data)
-															true);				//check overlaps*/
-		} else { 
+		
 		G4VPhysicalVolume *physicalWCPMT = new G4PVPlacement(pmt_rot,	//its rotation
 															PMTPosition,		//its position
 															logicWCPMT,			//its logical volume
@@ -296,8 +276,7 @@ G4cout<< " placing logicWCPMT: "<< thepmtname<< " at " << atankcollection << G4e
 															logicWCBarrel,		//its mother volume
 															false,				//no boolean operations
 															PMTID,				//ID for this PMT (=channelkey in data)
-															true);				//check overlaps*/
-		}
+															true);				//check overlaps
 	}
 	pmt_position_file.close();
 
